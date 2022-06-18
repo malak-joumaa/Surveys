a62b1f99b841514111631a9dd36310bcc6d6edba
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-18 08:37:39 +0300

    Larvel: Addind addSurvey api

--- surveys-laravel/app/Http/Controllers/AdminController.php
+++ surveys-laravel/app/Http/Controllers/AdminController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\survey;
 
 class AdminController extends Controller
 {
-    //
+    public function addSurvey (Request $request){
+        $user = Auth::user();
+        $survey = new survey;
+        $survey->title = $request->title;
+        $survey->save();
+        return response()->json([
+            "status" => "Success",
+            "survey" => $survey
+        ], 200);
+    }
 }
