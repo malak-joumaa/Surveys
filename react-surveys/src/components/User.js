66ee74ffd5ef121ed10ee486d2bfbd21a704911f
malak-joumaa <malak.joumaa.dev@gmail.com>
2022-06-19 01:28:18 +0300

    React: Getting surveys from database

--- react-surveys/src/components/User.js
+++ react-surveys/src/components/User.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect} from 'react'
 
 const User = () => {
+    
+    useEffect(() => {
+        const getTasks = async () => {
+          const surveysFromServer = await fetchSurveys();
+          console.log(surveysFromServer);
+        };
+        getTasks();
+      }, []);
+
+    //Get all surveys
+    const fetchSurveys = async () => {
+        try {
+          const res = await fetch("http://127.0.0.1:8000/api/admin/get_survey");
+          const data = await res.json();
+          return data;
+        } catch (err) {
+          console.log(err);
+        }
+      };
 
   return (
     <div>User</div>
